f158fc4f9bf3ca1d3e456b71f50d65ae879ad9aa
Paul-Emmanuel SOTIR <paulemmanuel51@gmail.com>
2015-03-30 21:49:58 +0200

    Added volume attribute to settings class

--- core/src/GDXGame/EndlessCastle/Settings.java
+++ core/src/GDXGame/EndlessCastle/Settings.java
@@ -9,6 +9,21 @@ public class Settings
 	public static void save() {
 		//TODO: implémenter ça
 	}
+	
+	public int getVolume() {
+		return _volume;
+	}
+
+	public void setVolume(int volume) {
+		if(volume <= 100 && volume >= 0)
+			_volume = volume;
+		else if(volume < 0)
+			_volume = 0;
+		else
+			_volume = 100;
+	}
+
+	private int _volume;
 }
 
 enum Difficulty 
