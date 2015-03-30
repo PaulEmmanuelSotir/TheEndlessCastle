b181a58e0a840b6957bd38dd6db6948f534bcbca
Paul-Emmanuel SOTIR <paulemmanuel51@gmail.com>
2015-03-30 20:47:28 +0200

    Renamed 'MainClass' class to 'TheEndlessCastle'

--- core/src/GDXGame/EndlessCastle/TheEndlessCastle.java
+++ core/src/GDXGame/EndlessCastle/TheEndlessCastle.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainClass extends ApplicationAdapter {
+public class TheEndlessCastle extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
--- desktop/src/GDXGame/EndlessCastle/desktop/DesktopLauncher.java
+++ desktop/src/GDXGame/EndlessCastle/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package GDXGame.EndlessCastle.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import GDXGame.EndlessCastle.MainClass;
+import GDXGame.EndlessCastle.TheEndlessCastle;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainClass(), config);
+		new LwjglApplication(new TheEndlessCastle(), config);
 	}
 }
