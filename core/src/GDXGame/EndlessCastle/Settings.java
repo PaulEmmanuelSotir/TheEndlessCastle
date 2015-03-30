e4fbe15f628a231304372076987a91bdea1cbf4f
Paul-Emmanuel SOTIR <paulemmanuel51@gmail.com>
2015-03-30 21:08:24 +0200

    Settings class created

---
+++ core/src/GDXGame/EndlessCastle/Settings.java
@@ -0,0 +1,20 @@
+package GDXGame.EndlessCastle;
+
+public class Settings
+{
+	public static void load() {
+		//TODO: implémenter ça
+	}
+	
+	public static void save() {
+		//TODO: implémenter ça
+	}
+}
+
+enum Difficulty 
+{
+	easy,
+	normal,
+	hard,
+	insane
+}
