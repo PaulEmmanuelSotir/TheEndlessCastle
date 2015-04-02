ee25a4d4ac904b4889057eca705b1e61b66bfc04
Paul-Emmanuel SOTIR <paulemmanuel51@gmail.com>
2015-04-02 17:56:37 +0200

    Created assets directory structure and added some ressources

---
+++ core/assets/shaders/rotatingRays.glsl
@@ -0,0 +1,9 @@
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+	vec2 p = (-iResolution.xy+2.0*fragCoord.xy)/iResolution.y;
+	vec2 q = vec2( atan(p.y,p.x), length(p) );
+	float f = smoothstep( -0.1, 0.1, sin(q.x*10.0 + iGlobalTime) );
+	vec3 col = mix( vec3(0.42,0.55,1.0), vec3(0.6,0.7,1.0), f ); 
+	
+	fragColor = vec4( col, 1.0 );
+}
