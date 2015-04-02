b39047ce4b93961a4f9b93a65625d0a45e47abaa
Paul-Emmanuel SOTIR <paulemmanuel51@gmail.com>
2015-04-02 18:54:42 +0200

    Component ArrayList and id string added to Entity objects

--- core/src/game/entitiesAndComponents/Entity.java
+++ core/src/game/entitiesAndComponents/Entity.java
@@ -1,14 +1,26 @@
 package game.entitiesAndComponents;
 
-import game.utils.Position;
+import java.util.ArrayList;
+import java.util.List;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import game.utils.Position;
 
 public abstract class Entity
 {
 	protected Entity(float x, float y, float width, float height) {
 		_position = new Position(x, y);
 		_bounds = new Rectangle(x - width / 2, y - height / 2, width, height);
+	    _components = new ArrayList<Component>();
 	}
+	
+	 public void render(SpriteBatch batch) {
+		 //TODO: implémenter ça
+	 }
+		  
+	 public void update(float delta) {
+		//TODO: implémenter ça
+	 }
 	
 	public Position getPosition() {
 		return _position;
@@ -25,7 +37,14 @@ public abstract class Entity
 	public void setBounds(Rectangle bounds) {
 		_bounds = bounds;
 	}
+	
+	public String toString() {
+		return _id + "_pos=" + _position;
+	}
 
+
+	private String _id;
 	private Position _position;
 	private Rectangle _bounds;
+	private List<Component> _components;
 }
