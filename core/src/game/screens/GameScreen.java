f4906b8575d02508391f8261252ab07e47da1cdc
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-04-11 13:52:14 +0200

    Added camera and overrided dispose() and resize() in GameScreen

--- core/src/game/screens/GameScreen.java
+++ core/src/game/screens/GameScreen.java
@@ -2,6 +2,7 @@ package game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.GL20;
 import game.TheEndlessCastle;
@@ -19,6 +20,9 @@ public class GameScreen extends ScreenAdapter
 		};
 		
 		_world = new World(_worldListener);
+		_camera = new OrthographicCamera(32f, 32f*ratio);
+		_camera.position.set(_camera.viewportWidth / 2f, _camera.viewportHeight / 2f, 0);
+		_camera.update();
 	}
 	
 	@Override
@@ -26,23 +30,38 @@ public class GameScreen extends ScreenAdapter
 		update(delta);
 		draw();
 	}
+	
+	@Override
+	public void resize (int width, int height) {
+		_camera.viewportWidth = 32f;
+		_camera.viewportHeight = 32f * height/width;
+		_camera.update();
+	}
+	
+	@Override
+	public void dispose() {
+	}
 
 	private void update(float deltaTime)
 	{
-		//TODO: implémenter ça
+		_camera.update();
+		_batch.setProjectionMatrix(_camera.combined);
+
+		_time += 1/60f;
+		//TODO: update world
 	}
 	
 	private void draw()
-	{		
-		//TODO: implémenter ça
-		
+	{
 		_batch.begin();
-		// ...
+		//TODO: render world
 		_batch.end();
 	}
 	
 	private TheEndlessCastle _game;
 	private SpriteBatch _batch;
+	private OrthographicCamera _camera;
+	private float _time;
 	
 	private World _world;
 	private World.WorldListener _worldListener;
