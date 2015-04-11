0471a98d25e43d88441f66d03b01b8bcd1d56bc7
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-04-11 14:19:13 +0200

    Adapted lava2.glsl fragment shader.

--- core/assets/shaders/lava2.glsl
+++ core/assets/shaders/lava2.glsl
@@ -1,8 +1,16 @@
-#define time iGlobalTime*0.1
+varying vec2 vTexCoord0;
+varying vec4 vColor;
+varying vec4 vCoord;
+
+uniform mat4 u_projTrans;
+uniform sampler2D u_texture;
+uniform float u_globalTime;
+
+#define time u_globalTime*0.1
 
 float hash21(in vec2 n){ return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453); }
-mat2 makem2(in float theta){float c = cos(theta);float s = sin(theta);return mat2(c,-s,s,c);}
-float noise( in vec2 x ){return texture2D(iChannel0, x*.01).x;}
+mat2 makem2(in float theta){ float c = cos(theta); float s = sin(theta); return mat2(c,-s,s,c); }
+float noise( in vec2 x ){ return texture2D(u_texture, x*.01).a; }
 
 vec2 gradn(vec2 p)
 {
@@ -17,6 +25,7 @@ float flow(in vec2 p)
 	float z=2.;
 	float rz = 0.;
 	vec2 bp = p;
+	
 	for (float i= 1.;i < 7.;i++ )
 	{
 		//primary flow speed
@@ -35,29 +44,29 @@ float flow(in vec2 p)
 		p += gr*.5;
 		
 		//add noise octave
-		rz+= (sin(noise(p)*8.)*0.5+0.5)/z;
+		rz+= (sin(noise(p)*7.)*0.5+0.5)/z;
 		
 		//blend factor (blending displaced system with base system)
 		//you could call this advection factor (.5 being low, .95 being high)
-		p = mix(bp,p,.65);
+		p = mix(bp,p,.77);
 		
 		//intensity scaling
-		z *= 1.5;
+		z *= 1.4;
 		//octave scaling
-		p *= 3.;
-		bp *= 3.9;
+		p *= 2.;
+		bp *= 1.9;
 	}
 	return rz;	
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+void main()
 {
-	vec2 p = fragCoord.xy / iResolution.xy-0.5;
-	p.x *= iResolution.x/iResolution.y;
-	p*= 3.;
+	//	TODO: prendre en compte la résolution pour conserver un ratio 1:1 sur la texture
+	vec2 p = vec2(vTexCoord0.x, -vTexCoord0.y - 1.);
+	p *= 5.;
 	float rz = flow(p);
 	
-	vec3 col = vec3(.2,0.07,0.01)/rz;
-	col=pow(col,vec3(1.4));
-	fragColor = vec4(col,1.0);
+	gl_FragColor = vec4( pow(vec3(.2,0.07,0.01)/rz, vec3(1.4)) , 1.0);
+//	gl_FragColor = vec4( vTexCoord0 , 0.0, 1.0);
+//	gl_FragColor = texture2D(u_texture, p);
 }
