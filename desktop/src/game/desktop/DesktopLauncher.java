7e599ff2afa022c23bc22f7739ccd2546f15958e
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-04-11 15:14:30 +0200

    Added temporary window icon

--- desktop/src/game/desktop/DesktopLauncher.java
+++ desktop/src/game/desktop/DesktopLauncher.java
@@ -1,14 +1,20 @@
 package game.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import game.TheEndlessCastle;
 
 public class DesktopLauncher
 {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-
+		
+		config.title = "The endless castle";
+		//config.fullscreen = true;
+		config.addIcon("icon.size32x32.png", FileType.Internal);
+		
 		new LwjglApplication(new TheEndlessCastle(), config);
 	}
 }
