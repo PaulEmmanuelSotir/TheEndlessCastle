be5da93fff79b277884e8d686f91a0bb002bac30
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-04-11 13:38:35 +0200

    Simplified and adapted 'rotatingRays.glsl' fragment shader

--- core/assets/shaders/rotatingRays.glsl
+++ core/assets/shaders/rotatingRays.glsl
@@ -1,9 +1,14 @@
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+varying vec2 vTexCoord0;
+varying vec4 vColor;
+varying vec4 vCoord;
+
+uniform mat4 u_projTrans;
+uniform sampler2D u_texture;
+uniform float u_globalTime;
+
+void main()
 {
-	vec2 p = (-iResolution.xy+2.0*fragCoord.xy)/iResolution.y;
-	vec2 q = vec2( atan(p.y,p.x), length(p) );
-	float f = smoothstep( -0.1, 0.1, sin(q.x*10.0 + iGlobalTime) );
-	vec3 col = mix( vec3(0.42,0.55,1.0), vec3(0.6,0.7,1.0), f ); 
-	
-	fragColor = vec4( col, 1.0 );
+	vec2 p = vTexCoord0 - 0.5;
+	float f = smoothstep(-0.1, 0.1, sin(atan(p.y,p.x)*10.0 + u_globalTime));
+	gl_FragColor = vec4(mix( vec3(1.0f, 0.5f, 0.125f), vec3(1.,1.,1.0), f)*(1.0 - 0.4*length(p)), 1.);
 }
