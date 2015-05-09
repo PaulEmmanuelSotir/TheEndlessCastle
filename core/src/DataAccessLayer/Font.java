8d918b01eab7e21caa7dda59f2e77c2f8066a78a
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-05-10 00:40:30 +0200

    Created empty Font and Shader classes for future asset loading/pooling mechanism.

---
+++ core/src/DataAccessLayer/Font.java
@@ -0,0 +1,18 @@
+package DataAccessLayer;
+
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * TODO: implémenter la classe Font
+ */
+public class Font  implements Disposable
+{
+	public Font() {
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
---
+++ core/src/DataAccessLayer/Shader.java
@@ -0,0 +1,19 @@
+package DataAccessLayer;
+
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * TODO: implémenter la classe shader
+ */
+public class Shader implements Disposable
+{
+	public Shader() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
