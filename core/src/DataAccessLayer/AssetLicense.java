8dc8f61e2baaa18bb522367784f30970787b76e6
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-05-10 19:08:33 +0200

    Created License class

---
+++ core/src/DataAccessLayer/AssetLicense.java
@@ -0,0 +1,21 @@
+package DataAccessLayer;
+
+public class AssetLicense
+{
+	public AssetLicense(String licenseShortName, String licenseSummary, String hyperlink) {
+		LicenseShortName = licenseShortName;
+		LicenseSummary = licenseSummary;
+		Hyperlink = hyperlink;
+		
+	}
+	
+	public AssetLicense(String licenseShortName, String licenseSummary) {
+		LicenseShortName = licenseShortName;
+		LicenseSummary = licenseSummary;
+		Hyperlink = null;
+	}
+
+	public String LicenseShortName;
+	public String LicenseSummary;
+	public String Hyperlink;
+}
