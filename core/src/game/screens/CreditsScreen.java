f1babd5e678f8db3bcf86075cc7eea7a0e013a99
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-05-11 13:03:36 +0200

    Created CreditsScreen class

---
+++ core/src/game/screens/CreditsScreen.java
@@ -0,0 +1,27 @@
+package game.screens;
+
+import game.TheEndlessCastle;
+import game.dataAccessLayer.AssetsHandler;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Credits screen
+ */
+public class CreditsScreen extends Screen
+{
+	public CreditsScreen(TheEndlessCastle game)
+	{
+		super(game);
+	}
+
+	@Override
+	protected void update() {
+		
+	}
+	
+	@Override
+	protected void draw() {
+		
+	}
+}
