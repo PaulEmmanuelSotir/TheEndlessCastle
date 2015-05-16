04f5f2dfa09bc2d8bf2ae14a47ee52002ee0a42e
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-05-16 16:39:21 +0200

    Added AssetsHandler reference to Entities

--- core/src/game/entities/Entity.java
+++ core/src/game/entities/Entity.java
@@ -10,11 +10,20 @@ import game.World;
 import game.components.Component;
 import game.components.IRenderableComponent;
 import game.components.IUpdateableComponent;
+import game.dataAccessLayer.AssetsHandler;
 import game.utils.Position;
 
 public abstract class Entity
 {
-	protected Entity(String name, Rectangle bounds, Position position) {
+	/**
+	 * Entity constructor
+	 * @param name
+	 * @param bounds
+	 * @param position
+	 * @param assetsHndlr
+	 */
+	protected Entity(String name, Rectangle bounds, Position position, AssetsHandler assetsHndlr) {
+		_assetsHndlr = assetsHndlr;
 		_name = name;
 		_position = position;
 		_bounds = bounds;
@@ -23,6 +32,10 @@ public abstract class Entity
 		_updateableComponents = new ArrayList<IUpdateableComponent>();
 	}
 
+	/**
+	 * Add given component to entity components
+	 * @param component
+	 */
 	protected void addComponent(Component component)
 	{
 		if(component != null)
@@ -36,11 +49,19 @@ public abstract class Entity
 
 	}
 
+	/**
+	 * Renders entity's renderable components
+	 * @param batch
+	 */
 	public void render(SpriteBatch batch) {
 		for(IRenderableComponent compo : _renderableComponents)
 			compo.render(batch);
 	}
 
+	/**
+	 * Updates entity's updateable components
+	 * @param world
+	 */
 	public void update(World world) {
 		for(IUpdateableComponent compo : _updateableComponents)
 			compo.update(world);;
@@ -67,6 +88,7 @@ public abstract class Entity
 	}
 
 	private String _name;
+	protected AssetsHandler _assetsHndlr;
 	protected Position _position;
 	protected Rectangle _bounds;
 	protected List<Component> _components;
