882d0f9f0ce887b60acafd6a56a3f8ad034322d0
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-05-18 13:37:15 +0200

    Modified music playlist class to play a static music name list

--- core/src/game/RandomMusicPlaylist.java
+++ core/src/game/RandomMusicPlaylist.java
@@ -1,21 +1,21 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import game.dataAccessLayer.AssetsHandler;
 import game.dataAccessLayer.TypedAssetDescriptor.AssetTypeEnum;
 
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.utils.Array;
 
 public class RandomMusicPlaylist
 {
-	private Array<Music> _musics;
-	private FinishedMusicListener _musicListener;
-	private AssetsHandler _assetsHndlr;
-
-
 	public RandomMusicPlaylist(AssetsHandler assetsHndlr){
 		_assetsHndlr = assetsHndlr;
-		_musics = _assetsHndlr.get(AssetTypeEnum.music);
+		_musics = new ArrayList<Music>();
+		for(String name : _GAME_MUSIC_NAMES)
+			_musics.add((Music)_assetsHndlr.get(name));
+		
 		_musicListener = new FinishedMusicListener();
 	}
 	
@@ -50,14 +50,26 @@ public class RandomMusicPlaylist
 
 	private void PlayRandomMusic() {
 		if(_musics != null)
-			if(_musics.size > 0)
+			if(_musics.size() > 0)
 			{
-				int random = (int) (Math.random() * _musics.size);
+				int random = (int) (Math.random() * _musics.size());
 				_playingMusic = _musics.get(random);
 				_playingMusic.setOnCompletionListener(_musicListener);
 				_playingMusic.play();
 			}
 	}
-	
+
+	private List<Music> _musics;
+	private FinishedMusicListener _musicListener;
+	private AssetsHandler _assetsHndlr;
 	private Music _playingMusic;
+	
+	private static final List<String> _GAME_MUSIC_NAMES;
+	static
+	{
+		_GAME_MUSIC_NAMES = new ArrayList<String>();
+		_GAME_MUSIC_NAMES.add("GameMusicKnight");
+		_GAME_MUSIC_NAMES.add("GameMusicTrailer");
+		_GAME_MUSIC_NAMES.add("GameMusicRapture");
+	}
 }
