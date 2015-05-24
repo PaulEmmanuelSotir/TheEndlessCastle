fb737a392aba92651a04d9cede88e453039920d1
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-05-24 07:13:29 +0200

    Re-Reshaped whole project

--- desktop/src/game/desktop/DesktopLauncher.java
+++ desktop/src/game/desktop/DesktopLauncher.java
@@ -1,12 +1,22 @@
 package game.desktop;
 
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import game.TheEndlessCastle;
 
-public class DesktopLauncher {
+public class DesktopLauncher
+{
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "The endless castle";
+		//config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
+		config.samples = 6;	
+		config.addIcon("icon.medium.png", FileType.Internal);
+		
 		new LwjglApplication(new TheEndlessCastle(), config);
 	}
 }
